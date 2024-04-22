5c05838d61beb585d52b11ee0ebdd37c0a0aa3a2
moro0215 <k023g1077@g.neec.ac.jp>
2024-04-22 14:36:22 +0900

    資料18ページまで
    資料18ページまで

---
+++ DirectXGame/Player.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+
+class Player {
+
+public:
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+private:
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+
+	//モデル
+	Model* model_ = nullptr;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+};
