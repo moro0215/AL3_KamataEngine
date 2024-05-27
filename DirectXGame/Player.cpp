a849118185e566eb4fff6409395c7858cc7718a3
moro0215 <k023g1077@g.neec.ac.jp>
2024-05-27 15:09:26 +0900

    資料21ページまで完了

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <numbers>
 #include <Input.h>
+#include <algorithm>
 
 // コンストラクタ
 Player::Player(){};
@@ -24,17 +25,48 @@ void Player::Update() {
 	//行列を定数バッファーに転送
 	worldTransform_.UpdateMatrix();
 
+	//左右の自キャラ角度テーブル
+	float destinationRotationYTable[] = {std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float> * 3.0f / 2.0f};
+	//状況に応じた角度の取得
+	float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrDirection_)];
+	//自キャラの角度を設定
+	worldTransform_.rotation_.y = destinationRotationY;
+
 	//移動入力
 	if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
 		//左右加速
 		myVector3 acceleration = {};
 		if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
+			//左移動中の右入力
+			if (velocity_.mValue.x < 0.0f) {
+				//速度と逆方向に入力中は急ブレーキ
+				velocity_.mValue.x *= (1.0f - kAttenuation);
+			}
 			acceleration.mValue.x += kAcceleration;
+			// 右向きに
+			if (lrDirection_ != LRDirection::kRight) {
+				lrDirection_ = LRDirection::kRight;
+			}
 		} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
+			// 右移動中の右入力
+			if (velocity_.mValue.x > 0.0f) {
+				// 速度と逆方向に入力中は急ブレーキ
+				velocity_.mValue.x *= (1.0f - kAttenuation);
+			}
 			acceleration.mValue.x -= kAcceleration;
+			//左向きに
+			if (lrDirection_ != LRDirection::kLeft) {
+				lrDirection_ = LRDirection::kLeft;
+			}
 		}
 		//加速減速
 		velocity_ += acceleration;
+		//速度制限
+		velocity_.mValue.x = std::clamp(velocity_.mValue.x, -kLimitRunSpeed, kLimitRunSpeed);
+
+	} else {
+		//移動減衰
+		velocity_.mValue.x *= (1.0f - kAttenuation);
 	}
 	//移動
 	worldTransform_.translation_ += velocity_;
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -21,8 +21,6 @@ public:
 	//描画処理
 	void Draw();
 
-
-
 private:
 	//ワールド変換データ
 	WorldTransform worldTransform_;
@@ -38,4 +36,14 @@ private:
 	//速度
 	myVector3 velocity_ = {};
 	static inline const float kAcceleration = 0.01f;
+	static inline const float kAttenuation = 0.05f;
+	//速度制限
+	static inline const float kLimitRunSpeed = 1.0f;
+
+	//左右
+	enum class LRDirection{
+		kRight,
+		kLeft,
+	};
+	LRDirection lrDirection_ = LRDirection::kRight;
 };
